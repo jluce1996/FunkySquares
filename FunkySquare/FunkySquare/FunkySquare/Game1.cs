4fd25470ed8ce017f4715de268d173ad33002c03
jluce1996 <jluce1996@gmail.com>
2013-10-02 08:15:39 -0400

    blank screen, random squares, static tv!!!!!!!!!!!!!!

--- FunkySquare/FunkySquare/FunkySquare/Game1.cs
+++ FunkySquare/FunkySquare/FunkySquare/Game1.cs
@@ -19,8 +19,11 @@ namespace FunkySquare
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D square;
-
-
+        Rectangle[] magicstuff = new Rectangle[100];
+        int CurrentScreen = 1;
+        Color[] colors = new Color[3] { Color.Black, Color.Gray, Color.White };
+        
+    
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -49,8 +52,14 @@ namespace FunkySquare
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             square = Content.Load<Texture2D>("square");
+            Random random = new Random();
             
-
+            for (int i = 0; i < 100; i++)
+            {
+                int RandX = random.Next(0, 700);
+                int RandY = random.Next(0, 400);
+               magicstuff[i] = new Rectangle(RandX, RandY, i + 1, i + 1);              
+            }
         }
 
         /// <summary>
@@ -74,7 +83,28 @@ namespace FunkySquare
                 this.Exit();
 
             // TODO: Add your update logic here
+            KeyboardState kb = Keyboard.GetState();
 
+            if (kb.IsKeyDown(Keys.D1))
+            {
+                CurrentScreen = 1;
+            }
+            else if (kb.IsKeyDown(Keys.D2))
+            {
+                CurrentScreen = 2;
+            }
+            else if (kb.IsKeyDown(Keys.D3))
+            {
+                CurrentScreen = 3;
+            }
+            else if (kb.IsKeyDown(Keys.D4))
+            {
+                CurrentScreen = 4;
+            }
+            else if (kb.IsKeyDown(Keys.D5))
+            {
+                CurrentScreen = 5;
+            }
             base.Update(gameTime);
         }
 
@@ -125,22 +155,38 @@ namespace FunkySquare
 
         public void DrawSquares()
         {
-            
-            
-           for( int i = 0; i < 100; i ++)
-            {
-               Random random = new Random();
-                int randX = random.Next(0, 800);
-                int randY = random.Next(0, 600);
-                Rectangle funkysquare = new Rectangle(randX, randY, 10, 10);
-                spriteBatch.Draw(square, funkysquare, Color.Black);
+
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {               
+                spriteBatch.Draw(square, magicstuff[i], Color.Yellow);
             }
-            
-
-
-
-
         }
+         public void DrawBlankScreen()
+        {
+            {
+                for (int x = 0; x < this.Window.ClientBounds.Width / 25; x++)
+                {
+                    for (int y = 0; y < this.Window.ClientBounds.Height / 25; y++)
+                    {
+                        spriteBatch.Draw(square, new Vector2(x * 25, y * 25), Color.Teal);
+                    }
+                }
+            }
+        }
+
+         public void DrawStatic()
+         {
+             Random rand = new Random();
+                for (int i = 0; i < 100000; i++)
+            {
+                int RandX = rand.Next(0, 800);
+                int RandY = rand.Next(0, 500);
+                Rectangle tv = new Rectangle(RandX, RandY, 5, 5);
+                    spriteBatch.Draw(square, tv, colors[i % 3]);
+                
+                }
+         }
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -152,10 +198,14 @@ namespace FunkySquare
             spriteBatch.Begin();
 
             // TODO: Add your drawing code here
-            // DrawBlankScreen();
-            //DrawCheckerboard();
-            //DrawRainbow();
-             DrawSquares();
+            switch (CurrentScreen)
+            {
+                case 1: DrawBlankScreen(); break;
+                case 2: DrawCheckerboard(); break;
+                case 3: DrawRainbow(); break;
+                case 4: DrawSquares(); break;
+                case 5: DrawStatic(); break;
+            }
 
             spriteBatch.End();
 
